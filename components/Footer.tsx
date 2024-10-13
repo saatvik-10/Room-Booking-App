3572086327fff5171041929c9e5c8494b8b933ae
saatvik-10 <saatvikmadan529@gmail.com>
2024-10-13 14:14:37 +0200

    fixes in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className='bg-blue-100 py-6'>
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
         <p className='text-center text-sm text-gray-800'>
-          &copy; {currYear} Bookit. All rights reserved.
+          &copy; {currYear} Roomify. All rights reserved.
         </p>
       </div>
     </footer>
