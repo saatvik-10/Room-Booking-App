3cb9ec7c92ac6c740f863f90e611dd0e3c1cccba
saatvik-10 <saatvikmadan529@gmail.com>
2024-10-13 13:42:27 +0200

    added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  const currYear = new Date().getFullYear();
+
+  return (
+    <footer className='bg-blue-100 py-6'>
+      <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+        <p className='text-center text-sm text-gray-800'>
+          &copy; {currYear} Bookit. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
