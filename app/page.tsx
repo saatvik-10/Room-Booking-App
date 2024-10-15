8e89e86d23a9eb92db7d5f562443ee15c920ee99
saatvik-10 <saatvikmadan529@gmail.com>
2024-10-15 06:51:55 +0200

    bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,6 @@ import RoomCard from '@/components/RoomCard';
 import rooms from '@/data/rooms.json';
 import Heading from '@/components/Heading';
 
-interface Room {
-  id: number;
-  user_id: number;
-  name: string;
-  description: string;
-  sqft: number;
-  capacity: number;
-  location: string;
-  address: string;
-  amenities: string;
-  availability: string;
-  price_per_hour: number;
-  image: string;
-}
-
 export default function Home() {
   return (
     <div>
