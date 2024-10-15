67f840e6e024f7e56d9a113ca54357598c64991d
saatvik-10 <saatvikmadan529@gmail.com>
2024-10-15 06:02:11 +0200

    added room cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+import RoomCard from '@/components/RoomCard';
+import rooms from '@/data/rooms.json';
+
+interface Room {
+  user_id: number;
+  name: string;
+  description: string;
+  sqft: number;
+  capacity: number;
+  location: string;
+  address: string;
+  amenities: string;
+  availability: string;
+  price_per_hour: number;
+  image: string;
+}
+
 export default function Home() {
   return (
     <div>
-        <h1>Roomify</h1>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (
+        <p>No rooms available</p>
+      )}
     </div>
   );
 }
