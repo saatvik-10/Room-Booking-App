9c8cf93b805b08a771a0d1df7340f9b60b8a2795
saatvik-10 <saatvikmadan529@gmail.com>
2024-10-15 06:45:54 +0200

    added heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import RoomCard from '@/components/RoomCard';
 import rooms from '@/data/rooms.json';
+import Heading from '@/components/Heading';
 
 interface Room {
   id: number;
@@ -19,6 +20,7 @@ interface Room {
 export default function Home() {
   return (
     <div>
+      <Heading title='Available Rooms' />
       {rooms.length > 0 ? (
         rooms.map((room) => <RoomCard room={room} />)
       ) : (
---
+++ components/Heading.tsx
@@ -0,0 +1,15 @@
+interface HeadingProps {
+  title: string;
+}
+
+const Heading: React.FC<HeadingProps> = ({ title }) => {
+  return (
+    <section className='bg-white mb-5 shadow px-4 py-4'>
+      <h1 className='text-2xl font-bold tracking-tight text-gray-900'>
+        {title}
+      </h1>
+    </section>
+  );
+};
+
+export default Heading;
