4fe1d47e25ba23605650ca2ea19ee7225804091e
saatvik-10 <saatvikmadan529@gmail.com>
2024-10-16 16:42:45 +0200

    fixes in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ const Footer = () => {
   const currYear = new Date().getFullYear();
 
   return (
-    <footer className='bg-blue-100 py-6'>
+    <footer className='py-6'>
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-        <p className='text-center text-sm text-gray-800'>
+        <p className='text-center text-md text-gray-800'>
           &copy; {currYear} Roomify. All rights reserved.
         </p>
       </div>
